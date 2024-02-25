9ddd43717a9a534b1d73d1ab179afebbc11c3e62
Hugo <hugo.contrerasr01@gmail.com>
2024-02-25 02:51:07 -0300

    Cambiar offset de coordenadas

--- src/App.js
+++ src/App.js
@@ -57,15 +57,15 @@ export default function Game() {
   const xIsNext= currentMove % 2 === 0;
   const currentSquares = history[currentMove];
   const locations= [
-    {row: 0, col: 0},
-    {row: 0, col: 1},
-    {row: 0, col: 2},
-    {row: 1, col: 0},
     {row: 1, col: 1},
     {row: 1, col: 2},
-    {row: 2, col: 0},
+    {row: 1, col: 3},
     {row: 2, col: 1},
     {row: 2, col: 2},
+    {row: 2, col: 3},
+    {row: 3, col: 1},
+    {row: 3, col: 2},
+    {row: 3, col: 3},
   ]
   
   function handlePlay(nextSquares)
